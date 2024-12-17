66e4761784b19a04c0f30438b07df1f3bfd7f28e
Rmatsurika1532 <puppy.hime4265@gmail.com>
2024-12-17 15:54:55 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<html lang='ja'>
+
+<head>
+    <meta charset=UTF-8>
+    <title>PHP kadai_007</title>
+</head>
+
+<body>
+    <?php
+    $cook_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($cook_data);
+
+    echo '<br>';
+
+    ?>
+</body>
